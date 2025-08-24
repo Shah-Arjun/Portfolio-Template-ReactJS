f598239b118669b5765c65c77d96eedf99a76647
Arjun <arjunsah856@gmail.com>
2025-08-25 03:29:55 +0545

    created MYWork folder and files and mounted in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './Components/Hero/Hero'
 import Abotu from './Components/About/About'
 import About from './Components/About/About'
 import Services from './Components/Services/Services'
+import MyWork from './Components/MyWork/MyWork'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Hero />              {/*   mounting hero section */}
       <About />              {/*   mounting about section */}
       <Services />              {/*   mounting Services section */}
+      <MyWork />              {/*   mounting MyWork section */}
     </div>
   )
 }
