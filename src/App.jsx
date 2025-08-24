cb07cbeda427b5ec4c3d2a1b68aeedd177980d07
Arjun <arjunsah856@gmail.com>
2025-08-24 13:23:31 +0545

    added about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Abotu from './Components/About/About'
+import About from './Components/About/About'
 
 function App() {
   return (
     <div>
       <Navbar />              {/*   mounting navbar */}
-      <Hero />              {/*   mounting hero */}
+      <Hero />              {/*   mounting hero section */}
+      <About />              {/*   mounting about section */}
     </div>
   )
 }
