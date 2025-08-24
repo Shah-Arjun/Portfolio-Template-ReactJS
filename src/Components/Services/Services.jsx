a1ed73c578d70cf1a80789a07e4f5ba9b1ccb751
Arjun <arjunsah856@gmail.com>
2025-08-24 16:00:47 +0545

    added services data

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Services.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
+import Services_Data from '../../assets/services_data'      // importing services data from assets whish is array of objects
+import arrow_icon from '../../assets/arrow_icon.svg'
 
 const Services = () => {
   return (
@@ -11,8 +13,20 @@ const Services = () => {
         <img src={theme_pattern} alt="theme_pattern_img" />
       </div>
 
+      <div className="services-container">
+        {Services_Data.map((service, index)=>{  //return whole div for each service
+            return <div className='services-fromat'> 
+                <h3>{service.s_no}</h3>          
+                <h2>{service.s_name}</h2>
+                <p>{service.s_desc}</p>
 
-
+                <div className='services-readmore'>
+                    <p>Read More</p>
+                    <img src={arrow_icon} alt="" />
+                </div>
+            </div>
+        })}
+      </div>
 
 
     </div>
