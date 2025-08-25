c0c2ebc20cfc362e194ec223ee45d819ab16fcc2
Arjun <arjunsah856@gmail.com>
2025-08-25 12:37:43 +0545

    footer section component made

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Footer.css'
+import footer_logo from '../../assets/footer_logo.svg'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer">
+        <div className="footer-top">
+            <div className="footer-top-left">
+                <img src={footer_logo} alt="" />
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
