824b9e56f22169c1a9cb39bdff0f8e54d64de7e8
Arjun <arjunsah856@gmail.com>
2025-08-25 13:34:58 +0545

    footer section complected

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,16 +1,38 @@
 import React from 'react'
 import './Footer.css'
 import footer_logo from '../../assets/footer_logo.svg'
+import user_icon from '../../assets/user_icon.svg'
 
 const Footer = () => {
   return (
     <div>
       <div className="footer">
+
         <div className="footer-top">
             <div className="footer-top-left">
                 <img src={footer_logo} alt="" />
+                <p>I am a passionate full-stack developer from Nepal, skilled in building modern, user-friendly web applications.</p>
+            </div>
+            <div className="footer-top-right">
+                <div className="footer-email-input">
+                    <img src={user_icon} alt="" />
+                    <input type="email" placeholder='Enter your email'/>
+                </div>
+                <div className="footer-subscribe">Subscribe</div>
             </div>
         </div>
+
+        <hr />
+
+        <div className="footer-bottom">
+            <p className="footer-botton-left">&copy; 2025 Arjun Shah. All rights reserved.</p>
+            <div className="footer-bottom-right">
+                <p>Term or Services</p>
+                <p>Privacy Policy</p>
+                <p>Connect with me</p>
+            </div>
+        </div>
+
       </div>
     </div>
   )
