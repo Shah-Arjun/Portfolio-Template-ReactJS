0506cb1b7b37c385a12a2d5af185f79c917a3a8f
Arjun <arjunsah856@gmail.com>
2025-08-26 14:13:12 +0545

    made open close nav menu functional

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./Navbar.css"; //external css for navbar
 import logo from "../../assets/logo.svg";
 import underline from "../../assets/nav_underline.svg";
@@ -9,14 +9,24 @@ import menu_close from "../../assets/menu_close.svg"
 function Navbar() {
 
 // to store the state og menu, initially is hone
-  const [menu, setMenu] = useState("home");       
+  const [menu, setMenu] = useState("home"); 
+  
+  // for open close menu button
+  const menuRef = useRef();
+
+  const openMenu = () => {
+    menuRef.current.style.right = "0";
+  }
+  const closeMenu = () => {
+    menuRef.current.style.right = "-350px";
+  }
 
   return (
     <div id="home" className="navbar">
       <img src={logo} alt="logo" />
-      <img src={menu_open} alt="" className="nav-mob-open"/>  {/* menu icon for small mobile */}
-      <ul className="nav-menu">
-        <img src={menu_close} alt="" className="nav-mob-close"/>    {/* menu close X icon for mobile */}
+      <img src={menu_open} alt="" className="nav-mob-open" onClick={openMenu} />  {/* menu icon for small mobile */}
+      <ul ref={menuRef} className="nav-menu">
+        <img src={menu_close} alt="" className="nav-mob-close" onClick={closeMenu} />    {/* menu close X icon for mobile */}
         <li><AnchorLink className="anchor-link" href="#home"><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{ menu === "home"?<img src={underline} /> : <></> }</li>   {/* menu home ho vane unerline image dekha ntr ntg*/}
         <li><AnchorLink className="anchor-link" offset={50} href="#about"><p onClick={()=>setMenu("about")}>About</p></AnchorLink>{ menu === "about"?<img src={underline} /> : <></> }</li>
         <li><AnchorLink className="anchor-link" offset={50} href="#services"><p onClick={()=>setMenu("services")}>Services</p></AnchorLink>{ menu === "services"?<img src={underline} /> : <></> }</li>
